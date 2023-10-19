a3a04e3680eef495379fa144bb2f7c0c7ad3c0e6
jpspeaks <jaycamillo@umbradigitalcompany.com>
2023-10-19 15:45:32 +0800

    Updated configs for PWA

--- next.config.js
+++ next.config.js
@@ -1,55 +1,34 @@
-// /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 
-// const isDev = process.env.NODE_ENV !== 'production'
+const isDev = process.env.NODE_ENV !== 'production'
 
-// const withPWA = require('@imbios/next-pwa')({
-//   dest: 'public',
-//   disable: process.env.NODE_ENV === 'development',
-//   exclude: [
-//     // add buildExcludes here
-//     ({ asset, compilation }) => {
-//       if (
-//         asset.name.startsWith('server/') ||
-//         asset.name.match(
-//           /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
-//         )
-//       ) {
-//         return true
-//       }
-//       if (isDev && !asset.name.startsWith('static/runtime/')) {
-//         return true
-//       }
-//       return false
-//     }
-//   ]
-// })
+const withPWA = require('@imbios/next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  exclude: [
+    // add buildExcludes here
+    ({ asset, compilation }) => {
+      if (
+        asset.name.startsWith('server/') ||
+        asset.name.match(
+          /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
+        )
+      ) {
+        return true
+      }
+      if (isDev && !asset.name.startsWith('static/runtime/')) {
+        return true
+      }
+      return false
+    }
+  ]
+})
 
-// module.exports = withPWA({
-//   // next.js config
-//   images: {
-//     dangerouslyAllowSVG: true,
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'res.cloudinary.com'
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'cdn.shopify.com'
-//       }
-//     ]
-//   },
-//   reactStrictMode: false
-// })
-
-module.exports = {
+module.exports = withPWA({
+  // next.js config
   images: {
-    dangerouslyAllowSVG: true,
+    // dangerouslyAllowSVG: true,
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com'
-      },
       {
         protocol: 'https',
         hostname: 'umbra-digital.sgp1.digitaloceanspaces.com'
@@ -57,4 +36,17 @@ module.exports = {
     ]
   },
   reactStrictMode: false
-}
+})
+
+// module.exports = {
+//   images: {
+//     // dangerouslyAllowSVG: true,
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'umbra-digital.sgp1.digitaloceanspaces.com'
+//       }
+//     ]
+//   },
+//   reactStrictMode: false
+// }
