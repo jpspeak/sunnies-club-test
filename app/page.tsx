a9d5bb8a172808cab92f62c937b09bd0db536ba2
jpspeaks <jaycamillo@umbradigitalcompany.com>
2023-10-24 12:11:56 +0800

    Removed Home page old design

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,5 @@
 import React from 'react'
-import MainContainer from './shared/components/MainContainer'
-import MainWrapper from './shared/components/MainWrapper'
-import Carousel from './homepage/components/Carousel'
-import ButtonLink from './shared/components/ButtonLink'
 
 export default function Home() {
-  return (
-    <>
-      <MainContainer>
-        <MainWrapper className='flex flex-col h-screen px-0'>
-          <div className='grow'>
-            <Carousel />
-          </div>
-          <div className='flex gap-2 px-4 py-10'>
-            <ButtonLink href='/signup'>SIGN UP</ButtonLink>
-            <ButtonLink href='/signin' className='px-5 w-max whitespace-nowrap'>
-              LOG IN
-            </ButtonLink>
-          </div>
-        </MainWrapper>
-      </MainContainer>
-    </>
-  )
+  return <></>
 }
