4e6d8238a7b506158734feacc5cde6d57648acd9
jpspeaks <jaycamillo@umbradigitalcompany.com>
2023-11-06 14:09:33 +0800

    Added secondary email description

--- app/account/account-settings/email-address/page.tsx
+++ app/account/account-settings/email-address/page.tsx
@@ -7,8 +7,14 @@ export default function EmailAddress() {
   return (
     <>
       <TopBar title='ACCOUNT SETTINGS' showBackNav />
-      <MainContainer className='flex flex-col px-4 pt-[50px] pb-[110px]'>
+      <MainContainer className='flex flex-col px-4 pt-[50px] pb-[75px]'>
         <UpdateSecEmailForm />
+        <p className='p-3 rounded bg-soft-black-50 text-soft-black-400 text-xxs'>
+          Start earning points with your Sunnies Studios in-store account. If
+          you did not register with your original account, please add the email
+          you used in-store under the secondary email address to automatically
+          score points.​
+        </p>
       </MainContainer>
     </>
   )
