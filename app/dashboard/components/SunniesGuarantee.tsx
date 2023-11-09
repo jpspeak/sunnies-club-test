965812591c3d8108393acf26f7c5546f01c45c8d
jpspeaks <jaycamillo@umbradigitalcompany.com>
2023-11-09 16:54:12 +0800

    Updated sunnies guarantee icon

--- app/dashboard/components/SunniesGuarantee.tsx
+++ app/dashboard/components/SunniesGuarantee.tsx
@@ -1,4 +1,5 @@
 import ButtonLink from '@/app/shared/components/ButtonLink'
+import SunniesGuaranteeIcon from '@/app/shared/components/icons/SunniesGuaranteeIcon'
 import Image from 'next/image'
 import React from 'react'
 
@@ -6,15 +7,7 @@ export default function SunniesGuarantee() {
   return (
     <div className='p-4 text-white shadow-md rounded-xl bg-[#263074] mt-4'>
       <h3 className='text-xl'>Sunnies Guarantee</h3>
-      <Image
-        src='/icons/warranty-logo.png'
-        width={60}
-        height={24}
-        quality={100}
-        style={{ objectFit: 'contain', width: 'auto', height: '24px' }}
-        alt='Warranty logo'
-        className='mx-auto mt-6'
-      />
+      <SunniesGuaranteeIcon className='mx-auto mt-6' />
       <p className='mt-4 text-center text-xxs'>
         Happy with your purchase? If not, you’re welcome to change your Sunnies
         Studios product within 90 days of purchase.
---
+++ app/shared/components/icons/SunniesGuaranteeIcon.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function SunniesGuaranteeIcon({
+  className
+}: {
+  className?: string
+}) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='41'
+      height='24'
+      viewBox='0 8 41 24'
+      fill='none'
+      className={className}
+    >
+      <path
+        d='M9.9768 29.32C14.9529 29.32 18.9868 25.2861 18.9868 20.31C18.9868 15.3339 14.9529 11.3 9.9768 11.3C5.00071 11.3 0.966797 15.3339 0.966797 20.31C0.966797 25.2861 5.00071 29.32 9.9768 29.32Z'
+        stroke='white'
+        strokeWidth='1.5'
+        strokeMiterlimit='10'
+        strokeLinecap='round'
+      />
+      <path
+        d='M39.5571 20.31C39.5571 25.29 35.5271 29.32 30.5471 29.32C25.5671 29.32 21.5371 25.29 21.5371 20.31C21.5371 15.33 25.5771 11.3 30.5571 11.3C31.5871 11.3 32.5771 11.47 33.4971 11.79'
+        stroke='white'
+        strokeWidth='1.5'
+        strokeMiterlimit='10'
+        strokeLinecap='round'
+      />
+      <path
+        d='M18.9863 20.31H21.5463'
+        stroke='white'
+        strokeWidth='1.5'
+        strokeMiterlimit='10'
+        strokeLinecap='round'
+      />
+      <path
+        d='M27.4766 19.57L30.2366 22.43L39.9666 13.24'
+        stroke='white'
+        strokeWidth='1.5'
+        strokeMiterlimit='10'
+        strokeLinecap='round'
+      />
+    </svg>
+  )
+}
