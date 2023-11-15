ee124520ee3b191c6b60c1f79f550927ad126d01
jpspeaks <jaycamillo@umbradigitalcompany.com>
2023-11-15 12:37:16 +0800

    Enable meta pixel

--- app/shared/hooks/useFPixel.ts
+++ app/shared/hooks/useFPixel.ts
@@ -4,10 +4,10 @@ import { useEffect } from 'react'
 
 type EventName = 'PageView'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const useFPixel = ({ eventName }: { eventName: EventName }) => {
   useEffect(() => {
-    // window.fbq('track', eventName)
+    window.fbq('track', eventName)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 }
 
