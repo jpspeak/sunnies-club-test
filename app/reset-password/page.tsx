3c62de132cac5af24770cc5ca49a64f4f619f19e
jpspeaks <jaycamillo@umbradigitalcompany.com>
2023-11-15 11:58:10 +0800

    Test

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -4,8 +4,8 @@ import React, { useState } from 'react'
 import MainContainer from '../shared/components/MainContainer'
 import ChangePasswordForm from './components/ResetPasswordForm'
 import ChangePasswordSuccessful from './components/ResetPasswordSuccessful'
-import TopNavBar from '../shared/components/TopNavbar'
 import LogoIcon from '../shared/components/icons/LogoIcon'
+import TopNavBar from '../shared/components/TopNavbar'
 
 export default function ResetPassword() {
   const [resetPassSuccessful, setResetPassSuccessful] = useState<boolean>(false)
