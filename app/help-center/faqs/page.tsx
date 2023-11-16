8021be0d97034b394f8f75b72d02f79d667e802e
jpspeaks <jaycamillo@umbradigitalcompany.com>
2023-11-16 11:18:13 +0800

    Updated faqs copy

--- app/help-center/faqs/page.tsx
+++ app/help-center/faqs/page.tsx
@@ -8,71 +8,69 @@ export default function FAQs() {
     <>
       <TopBar title='FAQS' showBackNav />
       <MainContainer className='flex flex-col pt-[60px] pb-[110px] px-4 text-sm'>
-        <p>What is&nbsp;Sunnies&nbsp;Club?​</p>
+        <p>What is Sunnies Club?​​</p>
 
         <ul className='pl-4 list-disc text-soft-black-400'>
           <li>
-            Sunnies&nbsp;Club is a loyalty program where members can earn points
-            through purchasing products from any&nbsp;Sunnies&nbsp;Studios
-            store. All members will also get access to preview new collections,
-            get a chance to be invited to&nbsp;exclusive events, and get
-            exclusive promos and limited-edition products.​
+            Sunnies Club is a loyalty program where members can earn points
+            through purchasing products from any Sunnies Studios store. All
+            members will also get access to preview new collections, get a
+            chance to be invited to exclusive events, and get exclusive promos
+            and limited-edition products.​
           </li>
         </ul>
 
         <p>&nbsp;</p>
 
-        <p>How do I join?​</p>
+        <p>How do I join?​​</p>
 
         <ul className='pl-4 list-disc text-soft-black-400'>
           <li>
             <p>
-              Sign up at any&nbsp;Sunnies&nbsp;Studios store or
-              in&nbsp;sunniesclub.com. Make sure to use the same email address
-              as the one&nbsp;you registered in your&nbsp;Sunnies&nbsp;Studios
-              account to start earning points.​
+              Sign up at any Sunnies Studios store or in sunniesclub.com. Make
+              sure to use the same email address as the one you registered in
+              your Sunnies Studios account to start earning points.​
             </p>
           </li>
         </ul>
 
         <p>&nbsp;</p>
 
-        <p>How do I earn points?​</p>
+        <p>How do I earn points?​​</p>
 
         <ul className='pl-4 list-disc text-soft-black-400'>
           <li>
             <p>
-              Members can earn points through purchasing products from
-              any&nbsp;Sunnies&nbsp;Studios store. Get a point for
-              every&nbsp;P100 spent.​
+              Members can earn points through purchasing products from any
+              Sunnies Studios store. Get a point for every P100 spent.​
             </p>
           </li>
         </ul>
 
         <p>&nbsp;</p>
 
-        <p>How do I convert my points to vouchers?​</p>
+        <p>How do I convert my points to vouchers?​​</p>
 
         <ul className='pl-4 list-disc text-soft-black-400'>
           <li>
             <p>
               Go to the redeem section and select the voucher that you want to
-              claim. Once&nbsp;you have&nbsp;redeemed a voucher,&nbsp;show your
-              reward code to any of our staff in-store and&nbsp;they
-              will&nbsp;discount it from your purchase.​
+              claim. Once you have redeemed a voucher, show your reward code to
+              any of our staff in-store and they will discount it from your
+              purchase.​
             </p>
           </li>
         </ul>
 
         <p>​</p>
 
-        <p>Do my points and rewards expire?​</p>
+        <p>Do my points and rewards expire?​​</p>
 
         <ul className='pl-4 list-disc text-soft-black-400'>
           <li>
             <p>
-              All&nbsp;Sunnies&nbsp;Club points and rewards&nbsp;do
-              not&nbsp;have an expiration date.​
+              All Sunnies Club points and rewards do not have an expiration
+              date.​
             </p>
           </li>
         </ul>
@@ -80,44 +78,72 @@ export default function FAQs() {
         <p>&nbsp;</p>
 
         <p>
-          Can I use my&nbsp;Sunnies&nbsp;Club reward vouchers with other
-          promos?​
+          Can I use my Sunnies Club reward vouchers with other promos and sale
+          items?​
         </p>
 
         <ul className='pl-4 list-disc text-soft-black-400'>
           <li>
             <p>
-              Yes, all converted&nbsp;Sunnies&nbsp;Club vouchers can be used
-              together with other discounts or promos, but
-              reward&nbsp;vouchers&nbsp;cannot&nbsp;be combined or stacked for
-              use in a single transaction.​
+              Yes, all converted Sunnies Club vouchers can be used together with
+              other discounts or promos, but reward vouchers cannot be combined
+              or stacked for use in a single transaction.​
             </p>
           </li>
         </ul>
 
         <p>&nbsp;</p>
 
-        <p>Can I convert my points to cash?​</p>
+        <p>Can I convert my points to cash?​​​</p>
 
         <ul className='pl-4 list-disc text-soft-black-400'>
           <li>
             <p>
-              No, all&nbsp;Sunnies&nbsp;Club points may only be converted to
-              vouchers which can be used as good as cash.​
+              No, all Sunnies Club points may only be converted to vouchers
+              which can be used as good as cash.​
             </p>
           </li>
         </ul>
 
         <p>&nbsp;</p>
 
-        <p>Can I use the voucher at&nbsp;sunniesstudios.com?​</p>
+        <p>Can I use the voucher at sunniesstudios.com?​​</p>
 
         <ul className='pl-4 list-disc text-soft-black-400'>
           <li>
             <p>
-              No, the vouchers can only be used in&nbsp;Sunnies&nbsp;Studios
-              stores. Please refer to our Terms and Conditions to
-              check&nbsp;which stores it&nbsp;doesn&rsquo;t&nbsp;apply to.​
+              No, the vouchers can only be used in Sunnies Studios stores.
+              Please refer to our Terms and Conditions to check which stores it
+              doesn&apos;t apply to.​
+            </p>
+          </li>
+        </ul>
+
+        <p>&nbsp;</p>
+
+        <p>Do I use the same account as my Sunnies Studios account?​</p>
+
+        <ul className='pl-4 list-disc text-soft-black-400'>
+          <li>
+            <p>
+              Yes, please use the same email as the one registered in Sunnies
+              Studios account to start earning points. If you registered a
+              different email as your primary, you could add your Sunnies
+              Studios account as a secondary email to receive your points
+              automatically.​
+            </p>
+          </li>
+        </ul>
+
+        <p>&nbsp;</p>
+
+        <p>How can I get invited to exclusive events?​​</p>
+
+        <ul className='pl-4 list-disc text-soft-black-400'>
+          <li>
+            <p>
+              Check the announcement section of the Sunnies Club homepage for
+              event announcements and how to register.​
             </p>
           </li>
         </ul>
@@ -125,17 +151,28 @@ export default function FAQs() {
         <p>&nbsp;</p>
 
         <p>
-          Do I use the same account as my&nbsp;Sunnies&nbsp;Studios account?​
+          Can I use Sunnies Club vouchers to pay the price difference for
+          replacements under Sunnies Guarantee​​​
         </p>
 
         <ul className='pl-4 list-disc text-soft-black-400'>
           <li>
             <p>
-              Yes, please use the same email as the one registered in
-              your&nbsp;Sunnies&nbsp;Studios account to start earning points.
-              If&nbsp;you registered a different email as your primary, you
-              could add your&nbsp;Sunnies&nbsp;Studios account as a
-              secondary&nbsp;email to receive your points automatically.
+              No, all Sunnies Club vouchers can only be used when purchasing
+              products.​
+            </p>
+          </li>
+        </ul>
+
+        <p>&nbsp;</p>
+
+        <p>Do I still earn points when I purchase sale items?​</p>
+
+        <ul className='pl-4 list-disc text-soft-black-400'>
+          <li>
+            <p>
+              Yes, Sunnies Club points will be based on the amount that the
+              customer will pay.​
             </p>
           </li>
         </ul>
