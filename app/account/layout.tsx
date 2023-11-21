df5e0dfd9b7a222ff52a93f016788c8b5b686342
jpspeaks <jaycamillo@umbradigitalcompany.com>
2023-11-21 11:58:31 +0800

    Changed verification pixel event to lead

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,12 +1,7 @@
-'use client'
-
-import useFPixel from '../shared/hooks/useFPixel'
-
 export default function AccountLayout({
   children
 }: {
   children: React.ReactNode
 }) {
-  useFPixel({ eventName: 'PageView' })
   return children
 }
